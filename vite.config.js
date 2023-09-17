ddd4af1edc7656f3ad2572aff1c5196db0f4ca8f
Emirhankayar <emirhan.kayar80@gmail.com>
2023-09-18 00:32:56 +0200

    fixed rollups quick

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import json from '@rollup/plugin-json';
-import commonjs from 'vite-plugin-commonjs'; // Import the commonjs plugin
 
 export default defineConfig({
-  plugins: [react(), json(), commonjs()], // Add commonjs to your list of plugins
+  plugins: [react()],
 });
