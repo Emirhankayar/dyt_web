b7ba91fa3b06562cba4ed52645857cb03f21bad9
Emirhankayar <emirhan.kayar80@gmail.com>
2023-09-29 00:06:28 +0200

    code separating started

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,38 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 import PhoneInput from 'react-phone-number-input';
-import 'react-phone-number-input/style.css';
-import { Button, Typography } from "@material-tailwind/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload, faEye, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
-import Recaptcha from './Recaptcha';
-import { registerLocale, setDefaultLocale } from 'react-datepicker';
+import ReCAPTCHA from 'react-google-recaptcha';
 import tr from 'date-fns/locale/tr';
 import emailjs from 'emailjs-com';
-import { createClient } from '@supabase/supabase-js';
-import { format,addDays } from 'date-fns';
+import 'react-phone-number-input/style.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import { registerLocale, setDefaultLocale } from 'react-datepicker';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload, faEye, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { Button, Typography } from "@material-tailwind/react";
+import { supabaseClient as supabase, downloadPDF, viewPDF, useRecaptcha } from '../utils/utils';
 
-// TODO CHANGE DEFAULT DATE TO FIRST AVAILABLE DATE PROBABLY WITH MIN DATE??
-// TODO FIX ERROR MESSAGE LANGUAGES 
-// TODO FIX NAMING 
-// TODO DISABLE HOURS BEFORE TODAY TO PREVENT BUGS
-// TODO DEBUG PORTAL SIZE
+
+
+
+import { format, addDays } from 'date-fns';
 
 export default function Booking() {
   const serviceID = import.meta.env.VITE_SERVICE;
   const templateID = import.meta.env.VITE_TEMPLATE;
   const userID = import.meta.env.VITE_USER;
-  
+  const siteKey = import.meta.env.VITE_APP_SITE;
+
+
   registerLocale('tr', tr);
   setDefaultLocale('tr', tr);
-  
-  const [fullyBookedDates, setFullyBookedDates] = useState([]); 
-  const [selectedDateHours, setSelectedDateHours] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(addDays(new Date(), 1)); 
-  const [phoneNumber, setPhoneNumber] = useState('');
+
   const pdfFileName = 'Danisan_Bilgi_Formu.pdf';
-  const pdfFilePath = '/src/assets/';
+  const [appointmentsLoaded, setAppointmentsLoaded] = useState(false);
+  const [excludedTimes, setExcludedTimes] = useState([]);
+  const [fullyBookedDates, setFullyBookedDates] = useState([]);
+  const [allAppointments, setAllAppointments] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(addDays(new Date(), 1));
+  const [phoneNumber, setPhoneNumber] = useState('');
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -40,7 +41,6 @@ export default function Booking() {
     recaptchaValue: null,
   });
   
-
   const handleChange = (e) => {
     if (e && e.target) {
       const { name, value } = e.target;
@@ -52,81 +52,28 @@ export default function Booking() {
     setFormData({ ...formData, recaptchaValue: value });
   };
 
-  const getPdfUrl = (fileName) => {
-    return `${window.location.origin}${pdfFilePath}${fileName}`;
-  };
+  const recaptchaRef = useRecaptcha(handleRecaptchaChange); // Use the Recaptcha hook
 
-  const supabase_url = import.meta.env.VITE_SUPABASE_URL
-  const supabase_key = import.meta.env.VITE_SUPABASE
-  const supabase = createClient(supabase_url, supabase_key);
-  const getPdfUrlFromSupabase = async (fileName) => {
-    const pdfUrl = `${supabase_url}/storage/v1/object/public/pdf/${fileName}`;
+    // Example usage of downloadPDF
+  async function handleDownloadPdf() {
+    const pdfFileName = 'example.pdf';
+    await downloadPDF(pdfFileName);
+  }
 
-    // Check if the PDF file exists in the Supabase bucket
-    const response = await fetch(pdfUrl);
-
-    if (response.ok) {
-      return pdfUrl;
-    } else {
-      console.error('PDF not found in database.');
-      return null;
-    }
-  };
-
-  const handleOpenPdf = async () => {
-    const pdfUrl = await getPdfUrlFromSupabase(pdfFileName);
-    if (pdfUrl) {
-      // Create an anchor element with the target="_blank" attribute
-      const link = document.createElement('a');
-      link.href = pdfUrl;
-      link.target = '_blank';
-
-      // Trigger a click event to open the PDF in a new tab
-      link.click();
-    } else {
-      console.error('PDF not found in database.');
-    }
-  };
-
-  const handleDownloadPdf = async () => {
-    const pdfUrl = await getPdfUrlFromSupabase(pdfFileName);
-    if (pdfUrl) {
-      try {
-        const response = await fetch(pdfUrl);
-        const blob = await response.blob();
-
-        // Create a temporary link and trigger the download
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', pdfFileName);
-
-        // Trigger a click event to initiate the download
-        const clickEvent = new MouseEvent('click', {
-          view: window,
-          bubbles: true,
-          cancelable: true,
-        });
-        link.dispatchEvent(clickEvent);
-
-        // Clean up the temporary URL object
-        window.URL.revokeObjectURL(url);
-      } catch (error) {
-        console.error('Error downloading PDF:', error);
-      }
-    } else {
-      console.error('PDF not found in database.');
-    }
-  };
+  // Example usage of viewPDF
+  async function handleOpenPdf() {
+    const pdfFileName = 'example.pdf';
+    await viewPDF(pdfFileName);
+  }
 
   const handleBooking = async (e) => {
     e.preventDefault();
-  
+
     if (!formData.recaptchaValue) {
       alert('Lütfen reCAPTCHA doğrulamasını tamamlayın.');
       return;
     }
-  
+
     try {
       const { data, error } = await supabase.from('appointment').insert([
         {
@@ -136,15 +83,16 @@ export default function Booking() {
           phone: phoneNumber,
         },
       ]);
-  
+
       if (error) {
         console.error('Error inserting booking:', error);
         alert('Rezervasyonunuz oluşturulamadı.');
-        return; 
+        return;
       } else {
         console.log('Booking inserted successfully:', data);
         alert('Rezervasyonunuz başarıyla oluşturuldu!');
       }
+
 
       const formattedDate = format(selectedDate, 'dd/MM/yyyy HH:mm');
 
@@ -167,145 +115,125 @@ export default function Booking() {
         recaptchaValue: null,
       });
 
-      setPhoneNumber(''); // Reset phoneNumber to an empty string
-
-      // Reset the selectedDate or other related state variables if needed
-      setSelectedDate(new Date()); // Reset selectedDate to the initial value or another appropriate value
-  
+      setPhoneNumber(''); 
 
     } catch (error) {
       console.error('Error inserting booking or sending emails:', error);
       alert('Rezervasyonunuz oluşturulamadı.');
     }
   };
-  
+
   useEffect(() => {
-    const fetchBookedAppointments = async () => {
+    const fetchAllAppointments = async () => {
       try {
-        const { data, error } = await supabase
-          .from('appointment')
-          .select('created_at');
-  
+        const { data, error } = await supabase.from('appointment').select('created_at');
+
         if (error) {
-          console.error('Error fetching booked appointments:', error);
+          console.error('Error fetching appointments:', error);
           return;
         }
-  
-        const selectedDateHours = data
-          .map((appointment) => new Date(appointment.created_at))
-          .filter((date) => {
-            const dateYear = date.getFullYear();
-            const dateMonth = date.getMonth();
-            const dateDay = date.getDate();
-            return (
-              dateYear === selectedDate.getFullYear() &&
-              dateMonth === selectedDate.getMonth() &&
-              dateDay === selectedDate.getDate()
-            );
-          })
-          .map((date) => date.getHours());
-  
-        console.log('Selected Date Hours:', selectedDateHours);
-  
+
+        // Store all appointments in an array
+        const allAppointments = data.map((appointment) => ({
+          date: new Date(appointment.created_at),
+          hours: new Date(appointment.created_at).getHours(),
+        }));
+
+        // Set fully booked dates
         const fullyBookedDates = {};
-  
-        data.forEach((appointment) => {
-          const date = new Date(appointment.created_at).toDateString();
+        allAppointments.forEach((appointment) => {
+          const date = appointment.date.toDateString();
           if (!fullyBookedDates[date]) {
             fullyBookedDates[date] = 0;
           }
           fullyBookedDates[date]++;
         });
-  
+
         const fullyBookedDatesArray = Object.keys(fullyBookedDates).filter(
           (date) => fullyBookedDates[date] >= 10
         );
-  
-        // Set the fullyBookedDates and selectedDateHours
+
         setFullyBookedDates(fullyBookedDatesArray);
-        setSelectedDateHours(selectedDateHours);
-  
+
         // Now, call calculateStartDate with the populated fullyBookedDatesArray
         const calculatedFinalDate = calculateStartDate(fullyBookedDatesArray);
         setFinalDate(calculatedFinalDate);
+
+        // Set the selected date to the calculated final date
+        setSelectedDate(calculatedFinalDate);
+
+        // Set all appointments
+        setAllAppointments(allAppointments);
+
+        // Indicate that appointments have been loaded
+        setAppointmentsLoaded(true);
       } catch (error) {
-        console.error('Error fetching booked appointments:', error);
+        console.error('Error fetching appointments:', error);
       }
     };
-  
-    fetchBookedAppointments();
-  }, [selectedDate]);
-  
-  // Use useMemo to memoize the values
-  //const memoizedBookedHours = useMemo(() => bookedHours, []);
-  //const memoizedFullyBookedDates = useMemo(() => fullyBookedDates, []);
 
-  const fullyBookedDatesArray = fullyBookedDates.map((dateString) => new Date(dateString));
+    fetchAllAppointments();
+  }, []);
+
+  useEffect(() => {
+    if (appointmentsLoaded) {
+      // Generate and set the excluded times based on the selected date and allAppointments
+      setExcludedTimes(generateExcludedTimes(selectedDate, allAppointments));
+    }
+  }, [selectedDate, allAppointments, appointmentsLoaded]);
+
+  const fullyBookedDatesArray = useMemo(() => fullyBookedDates.map((dateString) => new Date(dateString)), [fullyBookedDates]);
 
   const calculateStartDate = (fullyBookedDatesArray) => {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
-  
+
     const tomorrow = new Date(today);
     tomorrow.setDate(tomorrow.getDate() + 1);
-  
+
     let startDate = new Date(tomorrow);
-  
+
     // Create a copy of the fullyBookedDatesArray to avoid overwriting
     const sortedBookedDates = [...fullyBookedDatesArray]
       .map((date) => new Date(date))
       .sort((a, b) => a - b);
-  
-    console.log('Fully Booked Dates Array:', sortedBookedDates);
-  
+
     for (let i = 0; i < sortedBookedDates.length; i++) {
       const fullyBookedDate = sortedBookedDates[i];
-  
-      console.log('Starting from:', startDate);
-      console.log('Checking date:', fullyBookedDate);
-  
       // Check if fully booked date is the same as the current startDate
       if (fullyBookedDate.getTime() === startDate.getTime()) {
         // Date is fully booked, check the next day
         startDate.setDate(startDate.getDate() + 1);
-        console.log('Date is fully booked, checking the next day:', startDate);
       } else if (fullyBookedDate > startDate) {
         // Found a fully booked day after tomorrow, setting startDate to that day
         startDate = fullyBookedDate;
-        console.log('Found a fully booked day after tomorrow, setting startDate to that day:', startDate);
-        break; // Exit the loop once the first available day is found
+        break; 
       }
     }
-  
-    console.log('Final startDate:', startDate);
-  
+
     return startDate;
   };
   
-  
+
   const [finalDate, setFinalDate] = useState(null);
-  
-  // Use the useEffect hook to call the calculation function once when the component mounts
-  useEffect(() => {
-    const calculatedFinalDate = calculateStartDate(fullyBookedDatesArray);
-    setFinalDate(calculatedFinalDate);
-  }, []);
 
-  const getHoursInRange = () => {
-    const hours = [];
-    
-    for (let i = 8; i <= 17; i++) {
-        // Convert single-digit hours to a string with leading zero (e.g., "08", "09")
-        const hourString = i < 10 ? `0${i}` : `${i}`;
-        
-        // Push the formatted hour to the array
-        hours.push(`${hourString}:00`);
-    }
-    
-    return hours;
-};
+  const generateExcludedTimes = (date, allAppointments) => {
+    const selectedDateDateString = date.toDateString();
 
-  
+    // Find appointments for the selected date
+    const appointmentsForSelectedDate = allAppointments.filter((appointment) => {
+      const appointmentDate = new Date(appointment.date);
+      return appointmentDate.toDateString() === selectedDateDateString;
+    });
+
+    // Generate an array of hours to be excluded based on existing appointments
+    const excludedTimes = appointmentsForSelectedDate.map((appointment) => {
+      const appointmentDate = new Date(appointment.date);
+      return new Date(appointmentDate.getFullYear(), appointmentDate.getMonth(), appointmentDate.getDate(), appointment.hours, 0, 0, 0);
+    });
+    return excludedTimes;
+  };
+
   return (
     <div className="container max-w-lg mx-auto p-1 bg-transparent rounded font-jet h-screen flex-col flex justify-center mt-12">
       <div className='container rounded-lg p-6 bg-gray-100 shadow-xl'>
@@ -341,7 +269,6 @@ export default function Booking() {
               value={formData.email}
               onChange={handleChange}
               required
-              filterTimes={getHoursInRange}
             />
           </div>
 
@@ -366,28 +293,31 @@ export default function Booking() {
               Tarih:
             </label>
             <DatePicker
-               className="w-full px-3 py-2 mt-2 border-gray-600 bg-white rounded-lg focus:outline-1 focus:border-gray-700"
+              className="w-full px-3 py-2 mt-2 border-gray-600 bg-white rounded-lg focus:outline-1 focus:border-gray-700"
               id='date'
               name='date'
               required
               placeholderText='Bir tarih seciniz'
-              onChange={(date) => {
-                setSelectedDate(date); // Update the selectedDate state
-              }}
+
               locale="tr"
+              onChange={(date) => {
+                setSelectedDate(date);
+
+                setExcludedTimes(generateExcludedTimes(date, allAppointments));
+              }}
               showTimeSelect
               timeFormat="HH:mm"
               timeIntervals={60}
               timeCaption="Saat"
               dateFormat="dd/MM/yyyy HH:mm"
               prevMonthButtonDisabled
-              selected={false}
+              selected={selectedDate}
               openToDate={finalDate}
               startDate={finalDate}
               minDate={finalDate}
-              minTime={new Date(selectedDate).setHours(8, 0, 0)} 
+              minTime={new Date(selectedDate).setHours(8, 0, 0)}
               maxTime={new Date(selectedDate).setHours(17, 0, 0)}
-              excludeTimes={selectedDateHours.map((hour) => new Date(selectedDate).setHours(hour, 0, 0, 0))}
+              excludeTimes={excludedTimes}
               excludeDates={fullyBookedDatesArray}
               withPortal
             />
@@ -424,7 +354,14 @@ export default function Booking() {
           </div>
 
           <div className="my-4">
-            <Recaptcha onRecaptchaChange={handleRecaptchaChange} />
+            <div className="mt-12 mb-4 sm:mt-0 md:mt-0 lg:mt-0">
+            <ReCAPTCHA
+                hl="tr"
+                sitekey={siteKey}
+                onChange={(value) => handleRecaptchaChange(value)}
+                ref={recaptchaRef}
+              />
+            </div>
           </div>
 
           <div className="mb-4">
---
+++ src/utils/utils.js
@@ -0,0 +1,86 @@
+import { createClient } from '@supabase/supabase-js';
+import { useRef, useEffect } from 'react';
+
+// Define Supabase client
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE;
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+const useRecaptcha = (onRecaptchaChange) => {
+    const recaptchaRef = useRef(null);
+  
+    useEffect(() => {
+      if (recaptchaRef.current) {
+        window.grecaptcha.ready(() => {
+
+        });
+      }
+    }, [onRecaptchaChange]);
+  
+    return recaptchaRef;
+  };
+  
+// Function to get the PDF URL from Supabase
+const getPdfUrlFromSupabase = async (fileName) => {
+  const pdfUrl = `${supabaseUrl}/storage/v1/object/public/pdf/${fileName}`;
+
+  // Check if the PDF file exists in the Supabase bucket
+  const response = await fetch(pdfUrl);
+
+  if (response.ok) {
+    return pdfUrl;
+  } else {
+    console.error('PDF not found in database.');
+    return null;
+  }
+};
+
+// Function to download PDF
+const downloadPDF = async (pdfFileName) => {
+  const pdfUrl = await getPdfUrlFromSupabase(pdfFileName);
+  if (pdfUrl) {
+    try {
+      const response = await fetch(pdfUrl);
+      const blob = await response.blob();
+
+      // Create a temporary link and trigger the download
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', pdfFileName);
+
+      // Trigger a click event to initiate the download
+      const clickEvent = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+      });
+      link.dispatchEvent(clickEvent);
+
+      // Clean up the temporary URL object
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error('Error downloading PDF:', error);
+    }
+  } else {
+    console.error('PDF not found in database.');
+  }
+};
+
+// Function to view PDF
+const viewPDF = async (pdfFileName) => {
+  const pdfUrl = await getPdfUrlFromSupabase(pdfFileName);
+  if (pdfUrl) {
+    // Create an anchor element with the target="_blank" attribute
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.target = '_blank';
+
+    // Trigger a click event to open the PDF in a new tab
+    link.click();
+  } else {
+    console.error('PDF not found in database.');
+  }
+};
+
+export { supabaseClient, downloadPDF, viewPDF, useRecaptcha };
