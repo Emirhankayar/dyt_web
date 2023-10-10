6c4e2f34af116dee331444a1c58a393754336e89
Emirhankayar <emirhan.kayar80@gmail.com>
2023-10-10 22:40:57 +0200

    doesnt worth to tinypng stuff

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -6,7 +6,6 @@ const About = React.lazy(() => import('../sections/AboutSec'));
 const BlogSec = React.lazy(() => import('../sections/BlogSec'));
 const RecipeSec = React.lazy(() => import('../sections/RecipeSec'));
 const BannerSec = React.lazy(() => import('../sections/BannerSec'));
-//const AlertComp = React.lazy(() => import('../components/Alert'));
 
 export default function MainPage() {
 
