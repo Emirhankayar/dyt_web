0ff5c95f6639740ed162352f80052934019e9bd8
Emirhankayar <emirhan.kayar80@gmail.com>
2023-10-10 22:06:16 +0200

    hello there vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import VitePages from 'vite-plugin-pages';
+import VitePagesSitemap from 'vite-plugin-pages-sitemap'; // Import the sitemap plugin
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePages(), // Add VitePages plugin
+    VitePagesSitemap(), // Add VitePagesSitemap plugin
+  ],
   build: {
     chunkSizeWarningLimit: 1000 * 1024, // Set the maximum chunk size to 1MB
   },
